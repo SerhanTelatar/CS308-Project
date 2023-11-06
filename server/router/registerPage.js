b4e74a24ac073e3eb4507955121bd81037b7c961
Serhan Emre Telatar <telatarserhan@gmail.com>
2023-11-06 17:30:51 +0300

    Remove comments

--- server/router/registerPage.js
+++ server/router/registerPage.js
@@ -1,16 +1,15 @@
 const express = require("express")
 const router = express.Router()
 const path = require('path')
-const admin = require("firebase-admin"); // Import the Firebase app instance
+const admin = require("firebase-admin");
 
 router.use(express.static(path.join(__dirname, 'public')))
 
-// Display the registration form
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'register.html'));
 })
 
-// Handle the registration form submission
+
 router.post("/", (req, res) => {
     const name = req.body.name;
     const username = req.body.username
@@ -22,18 +21,18 @@ router.post("/", (req, res) => {
             if (!snapshot.empty) {
                 res.send('Username already exists. Please choose another username.');
             } else {
-                // Username is available, proceed with registration
+
                 const newUser = {
                     name: name,
                     username: username,
                     password: password
-                    // Add other user details as needed
+
                 };
 
                 userCollection.add(newUser)
                     .then((docRef) => {
                         console.log('User registered with ID: ', docRef.id);
-                        res.redirect('/login'); // Redirect to the login page after successful registration
+                        res.redirect('/login');
                     })
                     .catch((error) => {
                         res.send('Registration failed: ' + error.message);
