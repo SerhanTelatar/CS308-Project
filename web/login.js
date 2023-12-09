449f365576fd732766151a66797305bf51e4e88d
elifozv <elifozvaris@sabanciuniv.edu>
2023-12-09 14:36:28 +0300

    added js files

---
+++ web/login.js
@@ -0,0 +1,42 @@
+// login.js
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.querySelector("form");
+  
+    loginForm.addEventListener("submit", async function (event) {
+      event.preventDefault(); 
+  
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
+
+      if (!username || !password) {
+        alert("Please fill in all fields.");
+        return;
+      }
+  
+      try {
+        const response = await fetch("/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+        });
+  
+        const data = await response.json();
+  
+        if (data.success) {
+          alert(data.message);
+          window.location.href = "/dashboard";
+        } else {
+          alert(data.message);
+        }
+      } catch (error) {
+        console.error("Login failed:", error.message);
+        alert("Login failed. Please try again.");
+      }
+    });
+  });
+  
--- web/register.js
+++
@@ -1,1 +0,0 @@
-// add button and fetch functions
